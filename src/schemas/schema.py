3d9f4aa7db34ce31cc6a2fa13c7047ef13f9fa98
Ogunsanu Oluwaseun <ogunsanu17@gmail.com>
2025-06-21 12:06:59 +0100

    better marshmallow import

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -1,55 +1,55 @@
-import marshmallow as ma
+from marshmallow import fields, Schema, validate
 from models import Post
     
-class UserSchema(ma.Schema):
-    id = ma.fields.Int(dump_only=True)
-    username = ma.fields.Str(required=True)
-    email = ma.fields.Str(required=True)
-    password = ma.fields.Str(required=True, load_only=True)
-    createdAt = ma.fields.DateTime(dump_only=True)
-    posts = ma.fields.List(ma.fields.Nested(lambda: PostSchema(only=("id", "title", "content"))), dump_only=True)
-    comments = ma.fields.List(ma.fields.Nested(lambda: CommentSchema(only=("content", "post"))), dump_only=True)
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    email = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+    createdAt = fields.DateTime(dump_only=True)
+    posts = fields.List(fields.Nested(lambda: PostSchema(only=("id", "title", "content"))), dump_only=True)
+    comments = fields.List(fields.Nested(lambda: CommentSchema(only=("content", "post"))), dump_only=True)
 
-class PostSchema(ma.Schema):
+class PostSchema(Schema):
     class Meta():
         model = Post
 
-    id = ma.fields.Int(dump_only=True)
-    category = ma.fields.Str(required=True)
-    content = ma.fields.Str(required=True)
-    tags = ma.fields.Str(required=True)
-    title = ma.fields.Str(required=True)
-    createdAt = ma.fields.DateTime(dump_only=True)
-    updatedAt = ma.fields.DateTime(dump_only=True)
-    user_id = ma.fields.Int(required=True, load_only=True)
-    user = ma.fields.Nested(UserSchema, only=("id","username"), dump_only=True)
-    comments = ma.fields.List(ma.fields.Nested(lambda: CommentSchema(only=("content", "user"))), dump_only=True)
-    comment_count = ma.fields.Int(dump_only=True)
+    id = fields.Int(dump_only=True)
+    category = fields.Str(required=True)
+    content = fields.Str(required=True)
+    tags = fields.Str(required=True)
+    title = fields.Str(required=True)
+    createdAt = fields.DateTime(dump_only=True)
+    updatedAt = fields.DateTime(dump_only=True)
+    user_id = fields.Int(required=True, load_only=True)
+    user = fields.Nested(UserSchema, only=("id","username"), dump_only=True)
+    comments = fields.List(fields.Nested(lambda: CommentSchema(only=("content", "user"))), dump_only=True)
+    comment_count = fields.Int(dump_only=True)
 
-class CommentSchema(ma.Schema):
-    id = ma.fields.Int(dump_only=True)
-    content = ma.fields.Str(required=True)
-    postedAt = ma.fields.DateTime(dump_only=True)
-    editedAt = ma.fields.DateTime(dump_only=True)
-    user_id = ma.fields.Int(required=True, load_only=True)
-    post_id = ma.fields.Int(required=True, load_only=True)
-    post = ma.fields.Nested(PostSchema, only=("id", "title", "content"), dump_only=True)
-    user = ma.fields.Nested(UserSchema, only=("id", "username"), dump_only=True)
+class CommentSchema(Schema):
+    id = fields.Int(dump_only=True)
+    content = fields.Str(required=True)
+    postedAt = fields.DateTime(dump_only=True)
+    editedAt = fields.DateTime(dump_only=True)
+    user_id = fields.Int(required=True, load_only=True)
+    post_id = fields.Int(required=True, load_only=True)
+    post = fields.Nested(PostSchema, only=("id", "title", "content"), dump_only=True)
+    user = fields.Nested(UserSchema, only=("id", "username"), dump_only=True)
 
-class PostUpdateSchema(ma.Schema):
-    category = ma.fields.Str()
-    tags = ma.fields.List(ma.fields.Str())
-    content = ma.fields.Str()
-    title = ma.fields.Str()
-    updatedAt = ma.fields.DateTime(dump_only=True)
+class PostUpdateSchema(Schema):
+    category = fields.Str()
+    tags = fields.List(fields.Str())
+    content = fields.Str()
+    title = fields.Str()
+    updatedAt = fields.DateTime(dump_only=True)
 
-class PostPutSchema(ma.Schema):
-    category = ma.fields.Str(required=True)
-    tags = ma.fields.List(ma.fields.Str(required=True))
-    content = ma.fields.Str(required=True)
-    title = ma.fields.Str(required=True)
-    updatedAt = ma.fields.DateTime(dump_only=True)
+class PostPutSchema(Schema):
+    category = fields.Str(required=True)
+    tags = fields.List(fields.Str(required=True))
+    content = fields.Str(required=True)
+    title = fields.Str(required=True)
+    updatedAt = fields.DateTime(dump_only=True)
 
-class PostQuerySchema(ma.Schema):
-    tags = ma.fields.Str()
-    term = ma.fields.Str()
+class PostQuerySchema(Schema):
+    tags = fields.Str()
+    term = fields.Str()
