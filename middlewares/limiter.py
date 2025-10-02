85f22cc5a5f11ae33d1103362baf1a407b44c769
Ogunsanu Oluwaseun <ogunsanu17@gmail.com>
2025-10-02 11:32:19 +0100

    exempt admin from rate limits

--- middlewares/limiter.py
+++ middlewares/limiter.py
@@ -1,7 +1,14 @@
+from flask_jwt_extended import get_jwt_identity
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from models import User
+from config.extensions import db
+from sqlalchemy import select
+
+current_user = db.session.scalars(select(User).where(User.id == int(get_jwt_identity()))).first()
 
 limiter = Limiter(
     key_func=get_remote_address,
     meta_limits=["2/hour, 4/day"],
+    application_limits_exempt_when=lambda: current_user.role == "admin" 
 )
